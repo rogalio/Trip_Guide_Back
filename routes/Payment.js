559daf88adbfe9693339d7cc290af85e167f9c92
Rogalio mouchel <you@example.com>
2022-12-06 17:50:26 +0100

    add flight payment

--- routes/Payment.js
+++ routes/Payment.js
@@ -33,4 +33,26 @@ router.post("/hotel/pay", async (req, res) => {
   }
 });
 
+router.post("/flight/pay", async (req, res) => {
+  try {
+    let { amount } = req.body;
+    amount = amount * 100;
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "EUR",
+    });
+    res.status(200).json({
+      message: "Payment successful",
+      clientSecret: payment.client_secret,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Server error",
+      success: false,
+    });
+  }
+});
+
 module.exports = router;
