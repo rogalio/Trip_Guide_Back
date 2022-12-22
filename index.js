235de4110b2f46651ac56f114b47f7ce63125076
Rogalio mouchel <you@example.com>
2022-12-22 16:59:29 +0100

    test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://192.168.1.19:3000",
   credentials: true,
 }));
 // test
