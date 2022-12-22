a39f2b54cebcf2f111e3ed740e61e813545dd272
Rogalio mouchel <you@example.com>
2022-12-22 11:55:13 +0100

    fixbug

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
   credentials: true,
 }));
 // test
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
